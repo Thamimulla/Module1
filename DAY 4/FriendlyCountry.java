60cf2ae5ec3322002196bd8aaa36afda5fee4125
Thamimulla <81641628+Thamimulla@users.noreply.github.com>
2021-04-07 17:07:21 +0530

    Add files via upload

---
+++ DAY 4/FriendlyCountry.java
@@ -0,0 +1,14 @@
+package p4;
+
+public class FriendlyCountry {
+
+	public static String countries[] =
+		{
+				"UAE",
+				"Nepal",
+				"Bhutan",
+				"Mauritius",
+				"Afganistan",
+				"Mangolia",
+		};
+}
